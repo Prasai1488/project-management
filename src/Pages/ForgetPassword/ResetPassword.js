67c64963869b2b14614e0a7d69c8acd1d9ed8f88
Prasai1488 <prasaiprithvi4@gmail.com>
2024-09-20 09:17:20 +0545

    Updated resetPAssword and OtpPage components

--- src/Pages/ForgetPassword/ResetPassword.js
+++ src/Pages/ForgetPassword/ResetPassword.js
@@ -1,16 +1,14 @@
-import { ErrorMessage, Field, Form, Formik } from "formik";
-import jwt_decode from "jwt-decode";
-import React, { useEffect, useState } from "react";
-import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
-import { useDispatch, useSelector } from "react-redux";
-
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
-
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import mlogo from "../../assets/mlogo.png";
 import Button from "../../Components/Buttons/Button";
 import TextField from "../../Components/TextField/TextField";
 import { resetPassword } from "../../Redux/Auth/thunk";
+import "./resetPassword.css";
 
 const ResetPasswordConfirm = (props) => {
   let history = useHistory();
@@ -64,82 +62,66 @@ const ResetPasswordConfirm = (props) => {
 
   return (
     <div className="login-bak">
-      <div className="account-pages my-5 reset-container">
-        <div className="container">
-          <div className="row justify-content-center ">
-            <div className="col-md-8 col-lg-6 col-xl-5">
-              <div className="card overflow-hidden card-reset">
-                <div className="bg-soft" style={{ backgroundColor: "#bf202f", color: "#ffffff" }}>
-                  <div className="row">
-                    <div className="col-sm-12">
-                      <div className="text-center m-3">
-                        <h5>New Password</h5>
-                      </div>
-                    </div>
-                    {/* #CE5661 */}
-                  </div>
-                </div>
-                <div className="card-body py-4">
-                  <div className="">
-                    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-                      {(formik) => {
-                        return (
-                          <Form autoComplete="off" className="form-horizontal ">
-                            <div className="mb-2 password-field">
-                              <div style={{ position: "relative" }}>
-                                <TextField
-                                  type={type}
-                                  name="password"
-                                  label="New Password"
-                                  placeholder="******"
-                                  className="login"
-                                  required
-                                  formikRequired={formik?.errors?.password && formik?.touched?.password}
-                                  onChange={(e) => formik.setFieldValue("password", e.target.value)}
-                                />
-                                <span className="fa-eye-button" onClick={handleClick}>
-                                  {type === "password" ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
-                                </span>
-                              </div>
-                            </div>
-                            <div className="mb-2 password-field">
-                              <div style={{ position: "relative" }}>
-                                <TextField
-                                  type={type1}
-                                  name="confirmNewPassword"
-                                  label="Confirm Password"
-                                  placeholder="******"
-                                  className="login"
-                                  required
-                                  formikRequired={
-                                    formik?.errors?.confirmNewPassword && formik?.touched?.confirmNewPassword
-                                  }
-                                  onChange={(e) => formik.setFieldValue("confirmNewPassword", e.target.value)}
-                                />
-                                <span className="fa-eye-button" onClick={handleClick1}>
-                                  {type1 === "password" ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
-                                </span>
-                              </div>
-                            </div>
-
-                            <div className="mt-3 d-flex justify-content-center align-items-center">
-                              <Button
-                                btnType="submit"
-                                className="btn create-button"
-                                createButton={true}
-                                title="Save"
-                                content="Save"
-                              />
-                            </div>
-                          </Form>
-                        );
-                      }}
-                    </Formik>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div className="reset-content">
+        <div className="white-box">
+          <div className="logo-container">
+            <img src={mlogo} alt="logo" className="main-logo" />
           </div>
+          <h2 className="form-title">New Password</h2>
+          <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            {(formik) => (
+              <Form autoComplete="off" className="form-horizontal">
+                <div className="mb-2 password-field">
+                  <div style={{ position: "relative" }}>
+                    <TextField
+                      type={type}
+                      name="password"
+                      label="New Password"
+                      placeholder="******"
+                      className="login"
+                      required
+                      formikRequired={formik?.errors?.password && formik?.touched?.password}
+                      onChange={(e) => formik.setFieldValue("password", e.target.value)}
+                    />
+                    <span className="fa-eye-button" onClick={handleClick}>
+                      {type === "password" ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                    </span>
+                  </div>
+                </div>
+                <div className="mb-2 password-field">
+                  <div style={{ position: "relative" }}>
+                    <TextField
+                      type={type1}
+                      name="confirmNewPassword"
+                      label="Confirm Password"
+                      placeholder="******"
+                      className="login"
+                      required
+                      formikRequired={formik?.errors?.confirmNewPassword && formik?.touched?.confirmNewPassword}
+                      onChange={(e) => formik.setFieldValue("confirmNewPassword", e.target.value)}
+                    />
+                    <span className="fa-eye-button" onClick={handleClick1}>
+                      {type1 === "password" ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                    </span>
+                  </div>
+                </div>
+                <div className="mt-3 d-flex justify-content-center align-items-center">
+                  <Button
+                    btnType="submit"
+                    className="btn create-buttonss"
+                    createButton={false}
+                    title="Save"
+                    content="Save"
+                  />
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+        <div className="copyright">
+          <span>Copyright © 2024</span>
+          <br />
+          <strong>Ranger Wholesale</strong>
         </div>
       </div>
     </div>
